807505937b44b32b7bc51c13b55f8287d573ae2d
Yoselyn Miranda <91152700+Yoselyn214@users.noreply.github.com>
2024-01-23 19:39:43 -0500

    Add files via upload

---
+++ AVL.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+struct Node{
+    int value;
+    Node* left;
+    Node* rigth;
+    Node(int dato):value(dato), left(nullptr),rigth(nullptr) {}
+
+};
+class AVL{
+    Node* raiz;
+    void insert();
+    void eliminar();
+    void buscar();
+    void 
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
