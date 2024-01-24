3c6a1a9d9fb4fbae01c0e6d101732ef003389d8c
FernandoUsurin <fernando.usurin@utec.edu.pe>
2024-01-23 19:32:43 -0500

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 template <typename T>
-struct  Node
+struct  NodeBT
 {
     T data;
-    Node* left;
-    Node* right;
-    Node() : left(nullptr), right(nullptr) {}
-    Node(T value): data(value),left(nullptr), right(nullptr) {}
+    NodeBT* left;
+    NodeBT* right;
+    NodeBT() : left(nullptr), right(nullptr) {}
+    NodeBT(T value): data(value),left(nullptr), right(nullptr) {}
     void Killself(){
         if(left){
             left->Killself();
@@ -23,25 +23,74 @@ struct  Node
 };
 
 template<typename T>
-class BST{
+class BSTree{
     private: 
-    Node* root;
+    NodeBT* root;
     public:
-    BST(): root(nullptr){};
-    void insert(T value){};
-    bool find(T value){};
-    string displayInOrder() {};
-    string displayPreOrder(){};
-    string displayPostOrder(){};
+    BSTree(): root(nullptr){};
+    void insert(NodeBT* node,T value){
+    if (node == nullptr)
+        node = new NodeBT<T>(value);
+    else if (value < node->data)
+        insert(node->left, value);
+    else
+        insert(node->right, value);  
+    };
+    bool find(NodeBT* node, T value){
+        if(node==nullptr){
+            return false;
+        }
+        else if (value<node->data){
+            return find(node->left, value);
+        }
+        else if (value>node->data){
+            return find(node->right, value);
+        }
+        else return true;
+    };
+    string displayInOrder(NodeBT<T> *node, string &result) {
+        if(node==nullptr){
+            return result
+        }
+        displayInOrder(node->left, result);
+        result+= node->data + " ";
+        displayInOrder(node->right, result);
+        return result;
+    };
+    string displayPreOrder(NodeBT<T> *node, string &result){
+        if(node==nullptr){
+            return result
+        }
+        result+= node->data + " ";
+        displayInOrder(node->left, result);
+        displayInOrder(node->right, result);
+        return result;
+    };
+    string displayPostOrder(NodeBT<T> *node, string &result){
+        if(node==nullptr){
+            return result
+        }
+        displayInOrder(node->left, result);
+        displayInOrder(node->right, result);
+        result+= node->data + " ";
+        return result;
+    };
     string displayBFS(){};
     string displayDFS(){};
     T minValue(){};
     T maxValue(){};
-    void remove(T value){};
+    void remove(T value){}{
+        if(find){
+
+        }
+        else{
+            cout<<"El nodo que quiere eliminar no esta en el BST"<<endl;
+        }
+    }
     int size(){};
     T predecessor(T value) {};
     T successor(T value){};
     void clear(){};
-    ~BST(){}
+    ~BSTree(){}
 
 };
