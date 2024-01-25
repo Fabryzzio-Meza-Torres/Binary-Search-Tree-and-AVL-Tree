0a87a6f63a99829d93dce533c74d7fbfe3577d48
Fabryzzio-Meza-Torres <fabryzziomt@gmail.com>
2024-01-24 21:37:56 -0500

    Advance 1

--- AVL.cpp
+++ AVL.cpp
@@ -1,51 +1,62 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-template<typename T>
-struct Node {
+template <typename T>
+struct Node
+{
     T value;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
     int altura;
     Node(T dato) : value(dato), altura(0), left(nullptr), right(nullptr) {}
 };
-template<typename T>
-class AVL {
+template <typename T>
+class AVL
+{
 private:
-    Node<T>* raiz;
-    string displayInOrder(Node<T> *node, string &result) {
-        if(node==nullptr){
+    Node<T> *raiz;
+    string displayInOrder(Node<T> *node, string &result)
+    {
+        if (node == nullptr)
+        {
             return result;
         }
         displayInOrder(node->left, result);
-        result+= node->data + " ";
+        result += node->data + " ";
         displayInOrder(node->right, result);
         return result;
     };
-    string displayPreOrder(Node<T> *node, string &result){
-        if(node==nullptr){
+    string displayPreOrder(Node<T> *node, string &result)
+    {
+        if (node == nullptr)
+        {
             return result;
         }
-        result+= node->data + " ";
+        result += node->data + " ";
         displayInOrder(node->left, result);
         displayInOrder(node->right, result);
         return result;
     };
-    string displayPostOrder(Node<T> *node, string &result){
-        if(node==nullptr){
+    string displayPostOrder(Node<T> *node, string &result)
+    {
+        if (node == nullptr)
+        {
             return result;
         }
         displayInOrder(node->left, result);
         displayInOrder(node->right, result);
-        result+= node->data + " ";
+        result += node->data + " ";
         return result;
     };
-    Node<T>* padre(Node<T>* temp, int dato){
-        if(temp == nullptr || raiz->value == dato){
+    Node<T> *padre(Node<T> *temp, int dato)
+    {
+        if (temp == nullptr || raiz->value == dato)
+        {
             return nullptr;
         }
         else if ((temp->right != nullptr && temp->right->value == dato) ||
-                 (temp->left != nullptr && temp->left->value == dato)) {
+                 (temp->left != nullptr && temp->left->value == dato))
+        {
             return temp;
         }
         if (dato < temp->value)
@@ -53,40 +64,52 @@ private:
         else
             return padre(temp->right, dato);
     }
-    int altura(Node<T>* temp) {
-        if (temp == nullptr) {
+    int altura(Node<T> *temp)
+    {
+        if (temp == nullptr)
+        {
             return 0;
-        } else {
+        }
+        else
+        {
             return 1 + max(altura(temp->left), altura(temp->right));
         }
     }
 
-    Node<T>* rotate_right(Node<T>*& th) {
-        Node<T>* temp = th->left;
+    Node<T> *rotate_right(Node<T> *&th)
+    {
+        Node<T> *temp = th->left;
         th->left = temp->right;
         temp->right = th;
         th = temp;
         return th;
     }
 
-    Node<T>* rotate_left(Node<T>*& th) {
-        Node<T>* temp = th;
+    Node<T> *rotate_left(Node<T> *&th)
+    {
+        Node<T> *temp = th;
         temp = th->right;
-        Node<T>* temp2 = th;
+        Node<T> *temp2 = th;
         th->right = temp2->left;
         temp2->left = th;
         th = temp;
         return th;
     }
 
-    void rotate(Node<T>*& th) {
-        if (balance_factor(th) > 1) {
-            if (balance_factor(th->left) < 0) {
+    void rotate(Node<T> *&th)
+    {
+        if (balance_factor(th) > 1)
+        {
+            if (balance_factor(th->left) < 0)
+            {
                 th->left = rotate_left(th->left);
             }
             th = rotate_right(th);
-        } else if (balance_factor(th) < -1) {
-            if (balance_factor(th->right) > 0) {
+        }
+        else if (balance_factor(th) < -1)
+        {
+            if (balance_factor(th->right) > 0)
+            {
                 th->right = rotate_right(th->right);
             }
             th = rotate_left(th);
@@ -94,60 +117,91 @@ private:
         th->altura = 1 + max(altura(th->left), altura(th->right));
     }
 
-    int balance_factor(Node<T>* temp) {
+    int balance_factor(Node<T> *temp)
+    {
         return altura(temp->left) - altura(temp->right);
     }
-    Node<T>* inserta(Node<T>*& th, int dato) {
-        if (th == nullptr) {
+    Node<T> *inserta(Node<T> *&th, int dato)
+    {
+        if (th == nullptr)
+        {
             return new Node(dato);
-        } else if (dato < th->value) {
+        }
+        else if (dato < th->value)
+        {
             th->left = inserta(th->left, dato);
-        } else if (dato > th->value) {
+        }
+        else if (dato > th->value)
+        {
             th->right = inserta(th->right, dato);
-        } else {
+        }
+        else
+        {
             return th;
         }
         th->altura = 1 + max(altura(th->left), altura(th->right));
         rotate(th);
         return th;
     }
-    Node<T>* search_Node(Node<T>* th, int dato) {
-        if (th == nullptr) {
+    Node<T> *search_Node(Node<T> *th, int dato)
+    {
+        if (th == nullptr)
+        {
             return nullptr;
-        } else if (th->value == dato) {
+        }
+        else if (th->value == dato)
+        {
             return th;
-        } else if (dato < th->value) {
+        }
+        else if (dato < th->value)
+        {
             return search_Node(th->left, dato);
-        } else {
+        }
+        else
+        {
             return search_Node(th->right, dato);
         }
     }
-    Node<T>* minValueNode(Node<T>* node) {
-        Node<T>* th = node;
-        while (th != nullptr && th->left != nullptr) {
+    Node<T> *minValueNode(Node<T> *node)
+    {
+        Node<T> *th = node;
+        while (th != nullptr && th->left != nullptr)
+        {
             th = th->left;
         }
         return th;
     }
 
-    Node<T>* removeNode(Node<T>* th, int dato) {
-        if (th == nullptr) {
+    Node<T> *removeNode(Node<T> *th, int dato)
+    {
+        if (th == nullptr)
+        {
             return nullptr;
         }
-        if (th->value > dato) {
+        if (th->value > dato)
+        {
             th->left = removeNode(th->left, dato);
-        } else if (th->value < dato) {
+        }
+        else if (th->value < dato)
+        {
             th->right = removeNode(th->right, dato);
-        } else {
-            if (th->left == nullptr || th->right == nullptr) {
-                Node<T>* temp = th->left ? th->left : th->right; // si temp no es nulo el puntero se asigna T->left, caso contrario T->right;
+        }
+        else
+        {
+            if (th->left == nullptr || th->right == nullptr)
+            {
+                Node<T> *temp = th->left ? th->left : th->right; // si temp no es nulo el puntero se asigna T->left, caso contrario T->right;
 
                 // Hoja
-                if (temp == nullptr) {
+                if (temp == nullptr)
+                {
                     delete th;
                     return nullptr;
-                } else {
-                    if(th == raiz){
+                }
+                else
+                {
+                    if (th == raiz)
+                    {
                         raiz = temp;
                     }
                     th->left = temp->left;
@@ -156,14 +210,17 @@ private:
                     th->altura = temp->altura;
                 }
                 delete temp;
-            } else {
-                //ambos hijos
-                Node<T>* temp = minValueNode(th->right); //trae el minimo valor del subarbol derecho del arbol
+            }
+            else
+            {
+                // ambos hijos
+                Node<T> *temp = minValueNode(th->right); // trae el minimo valor del subarbol derecho del arbol
                 th->value = temp->value;
                 th->right = removeNode(th->right, temp->value);
             }
         }
-        if(th == nullptr){ // este es para en caso de que el nodo luego de las operaciones quedara nulo retorne ese valor
+        if (th == nullptr)
+        { // este es para en caso de que el nodo luego de las operaciones quedara nulo retorne ese valor
             return nullptr;
         }
         th->altura = 1 + max(altura(th->left), altura(th->right));
@@ -172,30 +229,34 @@ private:
         return th; // Devuelve el puntero al nodo modificado
     }
 
-
 public:
     AVL() : raiz(nullptr) {}
-    bool remove(int dato){
-        return removeNode(raiz,dato) != nullptr;
+    bool remove(int dato)
+    {
+        return removeNode(raiz, dato) != nullptr;
     }
-    Node<T>* padree(int valor){
-        return padre(raiz,valor);
+    Node<T> *padree(int valor)
+    {
+        return padre(raiz, valor);
     }
 
-    void insert(int dato) {
+    void insert(int dato)
+    {
         raiz = inserta(raiz, dato);
     }
 
-    bool search(int dato) {
+    bool search(int dato)
+    {
         return search_Node(raiz, dato) != nullptr;
     }
 
-
-    ~AVL() {
+    ~AVL()
+    {
     }
 };
 
-int main() {
+int main()
+{
     AVL<int> arbol;
 
     arbol.insert(4);
@@ -205,17 +266,17 @@ int main() {
     arbol.insert(3);
     arbol.insert(5);
     arbol.insert(7);
-    Node<int>* padre = arbol.padree(7);
+    Node<int> *padre = arbol.padree(7);
     if (padre != nullptr)
         cout << "El padre de 7 es: " << padre->value << endl;
     else
         cout << "No tiene padre" << endl;
 
     int valor = 9;
-    cout << boolalpha << arbol.search(valor) << endl ;
+    cout << boolalpha << arbol.search(valor) << endl;
     cout << boolalpha << arbol.remove(valor) << endl;
     cout << boolalpha << arbol.search(valor) << endl;
-    //cout << "¿El valor " << valor << " esta en el arbol? " << (arbol.search(valor) ? "Sí" : "No") << endl;
+    // cout << "¿El valor " << valor << " esta en el arbol? " << (arbol.search(valor) ? "Sí" : "No") << endl;
 
     return 0;
 }
--- BST.cpp
+++ BST.cpp
@@ -4,142 +4,199 @@
 using namespace std;
 
 template <typename T>
-struct  NodeBT
+struct NodeBT
 {
     T data;
-    NodeBT* left;
-    NodeBT* right;
+    NodeBT *left;
+    NodeBT *right;
     NodeBT() : left(nullptr), right(nullptr) {}
-    NodeBT(T value): data(value),left(nullptr), right(nullptr) {}
-    void Killself(){
-        if(left){
+    NodeBT(T value) : data(value), left(nullptr), right(nullptr) {}
+    void Killself()
+    {
+        if (left)
+        {
             left->Killself();
         }
-        if (right){
+        if (right)
+        {
             right->Killself();
         }
         delete this;
     }
 };
 
-template<typename T>
-class BSTree{
-    private: 
-    NodeBT* root;
-    public:
-    BSTree(): root(nullptr){};
-    void insert(NodeBT* node,T value){
-    if (node == nullptr)
-        node = new NodeBT<T>(value);
-    else if (value < node->data)
-        insert(node->left, value);
-    else
-        insert(node->right, value);  
+template <typename T>
+class BSTree
+{
+private:
+    NodeBT *root;
+
+public:
+    BSTree() : root(nullptr){};
+    void insert(NodeBT *node, T value)
+    {
+        if (node == nullptr)
+            node = new NodeBT<T>(value);
+        else if (value < node->data)
+            insert(node->left, value);
+        else
+            insert(node->right, value);
     };
-    bool find(NodeBT* node, T value){
-        if(node==nullptr){
+    bool find(NodeBT *node, T value)
+    {
+        if (node == nullptr)
+        {
             return false;
         }
-        else if (value<node->data){
+        else if (value < node->data)
+        {
             return find(node->left, value);
         }
-        else if (value>node->data){
+        else if (value > node->data)
+        {
             return find(node->right, value);
         }
-        else return true;
+        else
+            return true;
     };
-    string displayInOrder(NodeBT<T> *node, string &result) {
-        if(node==nullptr){
+    string displayInOrder(NodeBT<T> *node, string &result)
+    {
+        if (node == nullptr)
+        {
             return result
         }
         displayInOrder(node->left, result);
-        result+= node->data + " ";
+        result += node->data + " ";
         displayInOrder(node->right, result);
         return result;
     };
-    string displayPreOrder(NodeBT<T> *node, string &result){
-        if(node==nullptr){
+    string displayPreOrder(NodeBT<T> *node, string &result)
+    {
+        if (node == nullptr)
+        {
             return result
         }
-        result+= node->data + " ";
+        result += node->data + " ";
         displayInOrder(node->left, result);
         displayInOrder(node->right, result);
         return result;
     };
-    string displayPostOrder(NodeBT<T> *node, string &result){
-        if(node==nullptr){
+    string displayPostOrder(NodeBT<T> *node, string &result)
+    {
+        if (node == nullptr)
+        {
             return result
         }
         displayInOrder(node->left, result);
         displayInOrder(node->right, result);
-        result+= node->data + " ";
+        result += node->data + " ";
         return result;
     };
     string displayBFS(){};
     string displayDFS(){};
-    T minValue(NodeBT<T> *node){
-        if(node==nullptr){
+    T minValue(NodeBT<T> *node)
+    {
+        if (node == nullptr)
+        {
             throw("The tree is empty");
         }
-        else if(node->left==nullptr){
+        else if (node->left == nullptr)
+        {
             return node->data;
         }
-        else{
+        else
+        {
             minValue(node->left);
         }
     };
-    T maxValue(NodeBT<T> *node){
-        if(node==nullptr){
+    T maxValue(NodeBT<T> *node)
+    {
+        if (node == nullptr)
+        {
             throw("The tree is empty");
         }
-        else if(node->right==nullptr){
+        else if (node->right == nullptr)
+        {
             return node->data;
         }
-        else{
+        else
+        {
             maxValue(node->right);
         }
     };
-    void remove(NodeBT<T> *&node, T value){}{
-        if(node==nullptr){
+    void remove(NodeBT<T> *&node, T value) {}
+    {
+        if (node == nullptr)
+        {
             return;
         }
-        else if(value<node->data){
+        else if (value < node->data)
+        {
             remove(node->left, value);
         }
-        else if(value>node->data){
+        else if (value > node->data)
+        {
             remove(node->right, value);
         }
-        else{
-            //Leaf
-            if(node->left == nullptr && node->right == nullptr){
+        else
+        {
+            // Leaf
+            if (node->left == nullptr && node->right == nullptr)
+            {
                 delete node;
-                node= nullptr
+                node = nullptr
             }
-            //One child
-            else if(node->left== nullptr){
-                NodeBT<T> temp= node;
+            // One child
+            else if (node->left == nullptr)
+            {
+                NodeBT<T> temp = node;
                 node = node->right;
                 delete temp;
             }
 
-            else if(node->right==nullptr){
-                NodeBT<T> temp= node;
+            else if (node->right == nullptr)
+            {
+                NodeBT<T> temp = node;
                 node = node->left;
                 delete temp;
             }
-            //Two child
-            else{
-                T temp= maxValue(node->left);
-                node->data= temp;
+            // Two child
+            else
+            {
+                T temp = maxValue(node->left);
+                node->data = temp;
                 remove(node->left, temp);
             }
-
         }
     }
-    int size(){};
-    T predecessor(T value) {};
+    int Tree_size()
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1 + size(root->left) + size(root->right);
+        }
+    }
+
+    int height(NodeBT<T> *node)
+    {
+        if (node == nullptr)
+        {
+            return 0;
+        }
+        else
+        {
+            return size(node->left) + size(node->right) + 1;
+        }
+    }
+    T predecessor(T value)
+    {
+        T temp = MaxValue(root->left);
+    }
     T successor(T value){};
     void clear(){};
-    ~BSTree(){}
-
+    ~BSTree() {}
 };
